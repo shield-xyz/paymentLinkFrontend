e1d66ecea1d82f73b987ad43f1e72f6394a4f030
Fabricio Richieri <fabriciorichieri@Fabricios-MBP.fibertel.com.ar>
2024-06-13 18:17:13 -0300

    recharts

--- src/components/Balance.jsx
+++ src/components/Balance.jsx
@@ -1,4 +1,5 @@
 import { Icons } from './Icons';
+import Chart from './Recharts/Chart';
 import Button from './ui/button';
 import Container from './ui/container';
 
@@ -10,16 +11,22 @@ const Balance = () => {
           Total Balance <Icons.chevronRight />
         </span>
         <div className="flex items-center justify-between">
-          <div className="">
-            <span className="text-3xl font-semibold">$3,200</span>
-            <span className="text-3xl font-semibold text-gray-400">.80</span>
+          <div className="flex items-baseline gap-2">
+            <span className="text-5xl font-semibold">$3,200</span>
+            <span className="text-5xl font-semibold text-gray-400">.80</span>
+            <div className="text-success flex items-baseline gap-1">
+              <Icons.arrowUpCompressed className="" />
+              <span>85.66%</span>
+            </div>
           </div>
           <Button className="px-4 py-3 text-xs">Withdraw</Button>
         </div>
       </div>
 
       {/* Graphic */}
-      <div className="h-48"></div>
+      <div className="h-96">
+        <Chart />
+      </div>
     </Container>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -43,6 +43,10 @@ module.exports = {
           DEFAULT: 'hsl(var(--card))',
           foreground: 'hsl(var(--card-foreground))',
         },
+        success: {
+          DEFAULT: 'hsl(var(--success))',
+          foreground: 'hsl(var(--success-foreground))',
+        },
       },
       screens: {
         xs: '375px',
