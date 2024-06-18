029dabd46dd69cef5e17db6e8bc76bb24c191ee4
unknown <luiscarchi23@gmail.com>
2024-06-18 15:36:53 -0400

    profile mostly done email field not working

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,6 +5,7 @@ import { Icons } from './Icons';
 import SearchBar from './Searchbar';
 
 const Nav = ({ session }) => {
+
   return (
     <nav className="fixed z-20 flex h-[var(--nav-height)] w-full items-center border-b border-gray-200 bg-background/70 backdrop-blur-sm">
       <div className="m-auto flex h-20 w-full items-center justify-between gap-4 px-4 pr-8 lg:pl-[calc(320px+2rem)]">
