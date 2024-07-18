9b459956d37494f3067293da798e4aa484585166
Fabricio Richieri <frichieri.dev@gmail.com>
2024-07-17 23:25:30 -0300

    remove create payment link cache

--- src/app/(create-payment-link)/create-payment-link/page.jsx
+++ src/app/(create-payment-link)/create-payment-link/page.jsx
@@ -1,5 +1,7 @@
 import { CreatePaymentLinkForm, getNetworks } from '@/features/payment-link';
 
+export const revalidate = 0;
+
 export default async function Page() {
   const networks = await getNetworks();
 
