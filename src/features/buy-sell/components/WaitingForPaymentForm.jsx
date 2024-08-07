8c5bca585269e5edeaa7ea0eafbf4b04f148ef6a
Kimuel Anqui <83416645+kimikoi123@users.noreply.github.com>
2024-08-07 11:57:01 +0800

    update spacings
    update spacings

--- src/features/buy-sell/components/WaitingForPaymentForm.jsx
+++ src/features/buy-sell/components/WaitingForPaymentForm.jsx
@@ -119,12 +119,13 @@ export const WaitingForPaymentForm = ({ handleChangeStep }) => {
       ) : (
         <div>
           <div className="text-black/30">Scan/Send Deposit</div>
-          <div className="flex items-center justify-center p-8">
-            <QRCode value={selectedNetwork.deposit_address} size={200} />
+          <div className="flex items-center justify-center p-4 sm:p-8">
+            <QRCode className='hidden sm:hidden' value={selectedNetwork.deposit_address} size={200} />
+            <QRCode className='block sm:hidden' value={selectedNetwork.deposit_address} size={180} />
           </div>
-          <div className="mt-4 flex justify-between gap-8">
+          <div className="mt-4 flex justify-between gap-4 sm:gap-8">
             <div className="text-black/60">Address</div>
-            <div className="flex gap-4">
+            <div className="flex gap-2 sm:gap-4">
               <div className="hidden sm:block">
                 {selectedNetwork.deposit_address}
               </div>
