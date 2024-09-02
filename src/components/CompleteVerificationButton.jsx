7f04ad8350a416de8786896ea69c26c8fd97213b
Fabricio Richieri <frichieri.dev@gmail.com>
2024-09-02 12:13:33 -0300

    redirect complete verification to /verify

--- src/components/CompleteVerificationButton.jsx
+++ src/components/CompleteVerificationButton.jsx
@@ -1,60 +1,22 @@
 'use client';
 
-import footprint from '@onefootprint/footprint-js';
 import '@onefootprint/footprint-js/dist/footprint-js.css';
-import { toast } from 'sonner';
-
-import { getUserAuthToken } from '@/actions/getUserAuthToken';
-import { handleSubmissionError } from '@/lib/utils';
+import Link from 'next/link';
 
 import { Button } from './ui/button';
 
 const CompleteVerificationButton = ({ session }) => {
-  const isVerified = session.user?.verify; //
+  console.log({ session });
+  const isVerified = session.user?.verify;
 
   if (isVerified) return null;
 
-  const handleComplete = async (validationToken) => {
-    // TODO: Post to Footprint server to fetch user ID and verification status
-    try {
-      console.log({ validationToken });
-      toast.info('Verification successful');
-      // const res = await onComplete(validationToken);
-      // if (res?.error) {
-      //   throw new Error(res.error);
-      // }
-      // toast.info('To be implemented');
-    } catch (error) {
-      handleSubmissionError(error);
-    }
-  };
-
-  const handleOpen = async () => {
-    try {
-      const res = await getUserAuthToken(session.accessToken);
-      if (!res.status === 'success') {
-        throw new Error('Something went wrong');
-      }
-      const { token } = res.response;
-
-      const component = footprint.init({
-        kind: 'verify',
-        authToken: token,
-        onComplete: (validationToken) => {
-          console.log({ validationToken });
-          handleComplete(validationToken);
-        },
-      });
-      component.render();
-    } catch (error) {
-      handleSubmissionError(error);
-    }
-  };
-
   return (
-    <Button onClick={handleOpen} title="Please verify to use the platform">
-      Complete verification
-    </Button>
+    <Link href="/verify">
+      <Button title="Please verify to use the platform">
+        Complete verification
+      </Button>
+    </Link>
   );
 };
 
